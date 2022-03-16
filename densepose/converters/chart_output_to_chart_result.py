fb0c98fa191ea2a8246ab3c487dd6b3619213e23
SenHe <senhe752@gmail.com>
2022-03-16 12:45:49 +0000

    added all initial files for im2uv2im

---
+++ densepose/converters/chart_output_to_chart_result.py
@@ -0,0 +1,193 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+
+from typing import Dict
+import torch
+from torch.nn import functional as F
+
+from detectron2.structures.boxes import Boxes, BoxMode
+
+from ..structures import (
+    DensePoseChartPredictorOutput,
+    DensePoseChartResult,
+    DensePoseChartResultWithConfidences,
+)
+from . import resample_fine_and_coarse_segm_to_bbox
+from .base import IntTupleBox, make_int_box
+
+
+def resample_uv_tensors_to_bbox(
+    u: torch.Tensor,
+    v: torch.Tensor,
+    labels: torch.Tensor,
+    box_xywh_abs: IntTupleBox,
+    im_size: tuple,
+) -> torch.Tensor: # changed line
+    """
+    Resamples U and V coordinate estimates for the given bounding box
+
+    Args:
+        u (tensor [1, C, H, W] of float): U coordinates
+        v (tensor [1, C, H, W] of float): V coordinates
+        labels (tensor [H, W] of long): labels obtained by resampling segmentation
+            outputs for the given bounding box
+        box_xywh_abs (tuple of 4 int): bounding box that corresponds to predictor outputs
+    Return:
+       Resampled U and V coordinates - a tensor [2, H, W] of float
+    """
+    x, y, w, h = box_xywh_abs
+    w = max(int(w), 1)
+    h = max(int(h), 1)
+    u_bbox = F.interpolate(u, (h, w), mode="bilinear", align_corners=False)
+    v_bbox = F.interpolate(v, (h, w), mode="bilinear", align_corners=False)
+
+    canvas_u = torch.zeros(1,25,im_size[1], im_size[0], device=u.device) # new line
+    canvas_v = torch.zeros(1,25,im_size[1], im_size[0], device=v.device) # new line
+    canvas_u[:,:,y:y+h,x:x+w] = u_bbox # new line
+    canvas_v[:,:,y:y+h,x:x+w] = v_bbox # new_line
+    uv = torch.zeros([2, im_size[1], im_size[0]], dtype=torch.float32, device=u.device)
+    for part_id in range(1, canvas_u.size(1)):
+        uv[0][labels == part_id] = canvas_u[0, part_id][labels == part_id]
+        uv[1][labels == part_id] = canvas_v[0, part_id][labels == part_id]
+    return uv
+
+
+def resample_uv_to_bbox(
+    predictor_output: DensePoseChartPredictorOutput,
+    labels: torch.Tensor,
+    box_xywh_abs: IntTupleBox,
+    im_size: tuple,
+) -> torch.Tensor:
+    """
+    Resamples U and V coordinate estimates for the given bounding box
+
+    Args:
+        predictor_output (DensePoseChartPredictorOutput): DensePose predictor
+            output to be resampled
+        labels (tensor [H, W] of long): labels obtained by resampling segmentation
+            outputs for the given bounding box
+        box_xywh_abs (tuple of 4 int): bounding box that corresponds to predictor outputs
+    Return:
+       Resampled U and V coordinates - a tensor [2, H, W] of float
+    """
+    return resample_uv_tensors_to_bbox(
+        predictor_output.u,
+        predictor_output.v,
+        labels,
+        box_xywh_abs,
+        im_size,
+    )
+
+
+def densepose_chart_predictor_output_to_result(
+    predictor_output: DensePoseChartPredictorOutput, boxes: Boxes
+) -> DensePoseChartResult:
+    """
+    Convert densepose chart predictor outputs to results
+
+    Args:
+        predictor_output (DensePoseChartPredictorOutput): DensePose predictor
+            output to be converted to results, must contain only 1 output
+        boxes (Boxes): bounding box that corresponds to the predictor output,
+            must contain only 1 bounding box
+    Return:
+       DensePose chart-based result (DensePoseChartResult)
+    """
+    assert len(predictor_output) == 1 and len(boxes) == 1, (
+        f"Predictor output to result conversion can operate only single outputs"
+        f", got {len(predictor_output)} predictor outputs and {len(boxes)} boxes"
+    )
+
+    boxes_xyxy_abs = boxes.tensor.clone()
+    boxes_xywh_abs = BoxMode.convert(boxes_xyxy_abs, BoxMode.XYXY_ABS, BoxMode.XYWH_ABS)
+    box_xywh = make_int_box(boxes_xywh_abs[0])
+
+    labels = resample_fine_and_coarse_segm_to_bbox(predictor_output, box_xywh).squeeze(0)
+    uv = resample_uv_to_bbox(predictor_output, labels, box_xywh)
+    return DensePoseChartResult(labels=labels, uv=uv)
+
+
+def resample_confidences_to_bbox(
+    predictor_output: DensePoseChartPredictorOutput,
+    labels: torch.Tensor,
+    box_xywh_abs: IntTupleBox,
+) -> Dict[str, torch.Tensor]:
+    """
+    Resamples confidences for the given bounding box
+
+    Args:
+        predictor_output (DensePoseChartPredictorOutput): DensePose predictor
+            output to be resampled
+        labels (tensor [H, W] of long): labels obtained by resampling segmentation
+            outputs for the given bounding box
+        box_xywh_abs (tuple of 4 int): bounding box that corresponds to predictor outputs
+    Return:
+       Resampled confidences - a dict of [H, W] tensors of float
+    """
+
+    x, y, w, h = box_xywh_abs
+    w = max(int(w), 1)
+    h = max(int(h), 1)
+
+    confidence_names = [
+        "sigma_1",
+        "sigma_2",
+        "kappa_u",
+        "kappa_v",
+        "fine_segm_confidence",
+        "coarse_segm_confidence",
+    ]
+    confidence_results = {key: None for key in confidence_names}
+    confidence_names = [
+        key for key in confidence_names if getattr(predictor_output, key) is not None
+    ]
+    confidence_base = torch.zeros([h, w], dtype=torch.float32, device=predictor_output.u.device)
+
+    # assign data from channels that correspond to the labels
+    for key in confidence_names:
+        resampled_confidence = F.interpolate(
+            getattr(predictor_output, key), (h, w), mode="bilinear", align_corners=False
+        )
+        result = confidence_base.clone()
+        for part_id in range(1, predictor_output.u.size(1)):
+            if resampled_confidence.size(1) != predictor_output.u.size(1):
+                # confidence is not part-based, don't try to fill it part by part
+                continue
+            result[labels == part_id] = resampled_confidence[0, part_id][labels == part_id]
+
+        if resampled_confidence.size(1) != predictor_output.u.size(1):
+            # confidence is not part-based, fill the data with the first channel
+            # (targeted for segmentation confidences that have only 1 channel)
+            result = resampled_confidence[0, 0]
+
+        confidence_results[key] = result
+
+    return confidence_results  # pyre-ignore[7]
+
+
+def densepose_chart_predictor_output_to_result_with_confidences(
+    predictor_output: DensePoseChartPredictorOutput, boxes: Boxes, im_size: tuple
+) -> DensePoseChartResultWithConfidences: # changed line
+    """
+    Convert densepose chart predictor outputs to results
+
+    Args:
+        predictor_output (DensePoseChartPredictorOutput): DensePose predictor
+            output with confidences to be converted to results, must contain only 1 output
+        boxes (Boxes): bounding box that corresponds to the predictor output,
+            must contain only 1 bounding box
+    Return:
+       DensePose chart-based result with confidences (DensePoseChartResultWithConfidences)
+    """
+    assert len(predictor_output) == 1 and len(boxes) == 1, (
+        f"Predictor output to result conversion can operate only single outputs"
+        f", got {len(predictor_output)} predictor outputs and {len(boxes)} boxes"
+    )
+
+    boxes_xyxy_abs = boxes.tensor.clone()
+    boxes_xywh_abs = BoxMode.convert(boxes_xyxy_abs, BoxMode.XYXY_ABS, BoxMode.XYWH_ABS)
+    box_xywh = make_int_box(boxes_xywh_abs[0])
+
+    labels = resample_fine_and_coarse_segm_to_bbox(predictor_output, box_xywh, im_size).squeeze(0) # changed line
+    uv = resample_uv_to_bbox(predictor_output, labels, box_xywh, im_size) # changed line
+    confidences = resample_confidences_to_bbox(predictor_output, labels, box_xywh)
+    return DensePoseChartResultWithConfidences(labels=labels, uv=uv, **confidences)
---
+++ im2uv2im.py
@@ -0,0 +1,219 @@
+import copy
+import cv2  
+import matplotlib
+import numpy as np
+import argparse
+import pickle
+import os
+
+from matplotlib import pyplot as plt
+
+matplotlib.use('TkAgg')
+
+
+def parse_iuv(result):
+    i = result['pred_densepose'][0].labels.cpu().numpy().astype(float)
+    uv = (result['pred_densepose'][0].uv.cpu().numpy() * 255.0).astype(float)
+    iuv = np.stack((uv[1, :, :], uv[0, :, :], i))
+    iuv = np.transpose(iuv, (1, 2, 0))
+    return iuv
+
+
+def parse_bbox(result):
+    return result["pred_boxes_XYXY"][0].cpu().numpy()
+
+
+def concat_textures(array):
+    texture = []
+    for i in range(4):
+        tmp = array[6 * i]
+        for j in range(6 * i + 1, 6 * i + 6):
+            tmp = np.concatenate((tmp, array[j]), axis=1)
+        texture = tmp if len(texture) == 0 else np.concatenate((texture, tmp), axis=0)
+    return texture
+
+
+def interpolate_tex(tex):
+    # code is adopted from https://github.com/facebookresearch/DensePose/issues/68
+    valid_mask = np.array((tex.sum(0) != 0) * 1, dtype='uint8')
+    radius_increase = 10
+    kernel = np.ones((radius_increase, radius_increase), np.uint8)
+    dilated_mask = cv2.dilate(valid_mask, kernel, iterations=1)
+    region_to_fill = dilated_mask - valid_mask
+    invalid_region = 1 - valid_mask
+    actual_part_max = tex.max()
+    actual_part_min = tex.min()
+    actual_part_uint = np.array((tex - actual_part_min) / (actual_part_max - actual_part_min) * 255, dtype='uint8')
+    actual_part_uint = cv2.inpaint(actual_part_uint.transpose((1, 2, 0)), invalid_region, 1,
+                               cv2.INPAINT_TELEA).transpose((2, 0, 1))
+    actual_part = (actual_part_uint / 255.0) * (actual_part_max - actual_part_min) + actual_part_min
+    # only use dilated part
+    actual_part = actual_part * dilated_mask
+
+    return actual_part
+
+
+def get_texture(im, iuv, bbox, tex_part_size=200):
+    # this part of code creates iuv image which corresponds
+    # to the size of original image (iuv from densepose is placed
+    # within pose bounding box).
+    im = im.transpose(2, 1, 0) / 255
+    image_w, image_h = im.shape[1], im.shape[2]
+    bbox[2] = bbox[2] - bbox[0]
+    bbox[3] = bbox[3] - bbox[1]
+    x, y, w, h = [int(v) for v in bbox]
+    bg = np.zeros((image_h, image_w, 3))
+    ## bounding box based densepose prediction
+    #bg[y:y + h, x:x + w, :] = iuv
+    ## image size based densepose prediction
+    bg = iuv
+    iuv = bg
+    iuv = iuv.transpose((2, 1, 0))
+    i, u, v = iuv[2], iuv[1], iuv[0]
+
+    # following part of code iterate over parts and creates textures
+    # of size `tex_part_size x tex_part_size`
+    n_parts = 24
+    texture = np.zeros((n_parts, 3, tex_part_size, tex_part_size))
+    
+    for part_id in range(1, n_parts + 1):
+        generated = np.zeros((3, tex_part_size, tex_part_size))
+
+        x, y = u[i == part_id], v[i == part_id]
+        # transform uv coodrinates to current UV texture coordinates:
+        tex_u_coo = (x * (tex_part_size - 1) / 255).astype(int)
+        tex_v_coo = (y * (tex_part_size - 1) / 255).astype(int)
+        
+        # clipping due to issues encountered in denspose output;
+        # for unknown reason, some `uv` coos are out of bound [0, 1]
+        tex_u_coo = np.clip(tex_u_coo, 0, tex_part_size - 1)
+        tex_v_coo = np.clip(tex_v_coo, 0, tex_part_size - 1)
+        
+        # write corresponding pixels from original image to UV texture
+        # iterate in range(3) due to 3 chanels
+        for channel in range(3):
+            generated[channel][tex_v_coo, tex_u_coo] = im[channel][i == part_id]
+        
+        # this part is not crucial, but gives you better results 
+        # (texture comes out more smooth)
+        if np.sum(generated) > 0:
+            generated = interpolate_tex(generated)
+
+        # assign part to final texture carrier
+        texture[part_id - 1] = generated[:, ::-1, :]
+    
+    # concatenate textures and create 2D plane (UV)
+    tex_concat = np.zeros((24, tex_part_size, tex_part_size, 3))
+    for i in range(texture.shape[0]):
+        tex_concat[i] = texture[i].transpose(2, 1, 0)
+    tex = concat_textures(tex_concat)
+
+    return tex
+
+def TransferTexture(TextureIm,im,IUV):
+
+    U = IUV[:,:,1]
+    V = IUV[:,:,2]
+    #
+    R_im = np.zeros(U.shape)
+    G_im = np.zeros(U.shape)
+    B_im = np.zeros(U.shape)
+    ###
+    for PartInd in range(1,25):    ## Set to xrange(1,23) to ignore the face part.
+        #import ipdb; ipdb.set_trace()
+        tex = TextureIm[PartInd-1,:,:,:].squeeze() # get texture for each part.
+        #####
+        R = tex[:,:,0]
+        G = tex[:,:,1]
+        B = tex[:,:,2]
+        ###############
+        x,y = np.where(IUV[:,:,0]==PartInd)
+        u_current_points = U[x,y]   #  Pixels that belong to this specific part.
+        v_current_points = V[x,y]
+        ##
+        r_current_points = R[((255-v_current_points)*199./255.).astype(int),(u_current_points*199./255.).astype(int)]*255
+        g_current_points = G[((255-v_current_points)*199./255.).astype(int),(u_current_points*199./255.).astype(int)]*255
+        b_current_points = B[((255-v_current_points)*199./255.).astype(int),(u_current_points*199./255.).astype(int)]*255
+        ##  Get the RGB values from the texture images.
+        R_im[IUV[:,:,0]==PartInd] = r_current_points
+        G_im[IUV[:,:,0]==PartInd] = g_current_points
+        B_im[IUV[:,:,0]==PartInd] = b_current_points
+    generated_image = np.concatenate((B_im[:,:,np.newaxis],G_im[:,:,np.newaxis],R_im[:,:,np.newaxis]), axis =2 ).astype(np.uint8)
+    #import ipdb; ipdb.set_trace()
+    BG_MASK = generated_image==0
+    generated_image[BG_MASK] = im[BG_MASK]  ## Set the BG as the old image.
+    return generated_image
+
+
+def main(args):
+
+    densepose_file = os.path.join(args.densepose_path, args.image_name[:-4]+'.pkl')
+    image_file = os.path.join(args.image_path, args.image_name)
+
+    target_file = os.path.join(args.densepose_path, args.target_name[:-4]+'.pkl')
+
+    #import ipdb; ipdb.set_trace()
+
+    with open(densepose_file, 'rb') as f:
+
+        results = pickle.load(f)
+
+    with open(target_file, 'rb') as f:
+
+        target = pickle.load(f)
+
+
+    #import ipdb; ipdb.set_trace() 
+    iuv = parse_iuv(results)
+
+
+    t_iuv = parse_iuv(target)
+
+    ## vis dense pose in bgr
+    #cv2.imwrite('source_densepose'+'.png', (iuv).astype(np.uint8))
+    #cv2.imwrite('target_densepose'+'.png', (t_iuv).astype(np.uint8))
+    ## vis densepose in rgb
+    cv2.imwrite('./output/source_densepose'+'.png', (iuv[:,:,::-1]).astype(np.uint8))
+    cv2.imwrite('./output/target_densepose'+'.png', (t_iuv[:,:,::-1]).astype(np.uint8))
+    bbox = parse_bbox(results)
+    image = cv2.imread(image_file)[:,:,::-1]
+
+    ## extract uv texture map
+    uv_texture = get_texture(image, iuv, bbox).transpose(1,0,2)
+    #uv_texture = uv_texture[:,:,::-1]
+    cv2.imwrite('./output/source_uvtexture'+'.png', (uv_texture*255).astype(np.uint8)[:,:,::-1])
+
+    ##texture transfer
+    #import ipdb; ipdb.set_trace()
+
+
+
+    texture_im = np.zeros([24,200,200,3])
+    for i in range(4):
+
+        for j in range(6):
+
+            texture_im[(6*i+j) , :,:,:] = uv_texture[ (200*j):(200*j+200), (200*i):(200*i+200) ,: ]
+
+    #import ipdb; ipdb.set_trace()
+
+
+    im  = np.zeros(iuv.shape)
+    #import ipdb; ipdb.set_trace()
+    transfer_image = TransferTexture(texture_im,im,t_iuv[:,:,::-1].astype(np.uint8))
+    cv2.imwrite('./output/transfer_image'+'.png', transfer_image)
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--image_path', type=str, default='./img', help='path to the image directory')
+    parser.add_argument('--densepose_path', type=str, default='./densepose', help='path to the densepose directory')
+    parser.add_argument('--image_name', type=str, default = '000226_0.jpg', help='image to be visualized')
+    parser.add_argument('--target_name', type=str, default = '001403_0.jpg', help='image to be visualized')
+
+    args = parser.parse_args()
+
+    main(args)
+
+
